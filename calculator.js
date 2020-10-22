1c1fab9bedacf32be800f0cc45c4e046801b231e
lmrabiah <lm.alrabiah@gmail.com>
2020-10-22 14:16:45 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
 // Define the numbers and the operation here
 
 // Write if-statements here
+
+let number1 = 7;
+let number2 = 5;
+let addition = "+";
+let subtraction = "-";
+let multiplication = "*";
+let division = "/";
+
+let opration = addition;
+
+if (opration === addition) {
+  console.log(`${number1} + ${number2} = ${number1 + number2}`);
+} else if (opration === multiplication) {
+  console.log(`${number1} * ${number2} = ${number1 * number2}`);
+} else if (opration === subtraction) {
+  console.log(`${number1} - ${number2} = ${number1 - number2}`);
+} else if (opration === division) {
+  console.log(`${number1} / ${number2} = ${number1 / number2}`);
+}
